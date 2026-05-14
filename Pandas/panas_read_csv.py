b32fa309aaa228cfd63cfe0c83f3742edf9d3508
doyeon789 <ldyeon789@naver.com>
2026-05-14 14:42:29 +0900

    data 추가 삭제

--- Pandas/panas_read_csv.py
+++ Pandas/panas_read_csv.py
@@ -15,3 +15,9 @@ print('index:', data.index)
 
 # df 값을 배열 형태로 출력
 print('index value:', data.index.values[0:100])
+
+# 새로운 컬럼 'Age_new'를 생성
+data['Age_new'] = 0
+
+# 데이터 삭제
+data.drop('Age', axis=1) # inplace를 권장하지 않음
