1cf0218c4d708ce468cf53760fc42ada7d80b3ed
doyeon789 <ldyeon789@naver.com>
2026-05-20 16:32:52 +0900

    feat: inner merge

--- Pandas/pandas_merge_conversion .py
+++ Pandas/pandas_merge_conversion .py
@@ -23,11 +23,11 @@ print('-'*50)
 
 # Inner Merge
 df1 = pd.DataFrame({'key':['K0','K1','K2','K3'],
-                    'A': ['A0', 'A1', 'A2'],
-                    'B': ['B0', 'B1', 'B2'] })
+                    'A': ['A0', 'A1', 'A2','A3'],
+                    'B': ['B0', 'B1', 'B2','B3'] })
 
 
-df1 = pd.DataFrame({'key':['K0','K1','K2'],
+df2 = pd.DataFrame({'key':['K0','K1','K2'],
                     'C': ['C0', 'C1', 'C2'],
                     'D': ['D0', 'D1', 'D2'] })
 
