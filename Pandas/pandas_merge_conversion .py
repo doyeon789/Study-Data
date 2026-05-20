b2f8b0f31e7eb55bc17386d97c46d4d916b8ada8
doyeon789 <ldyeon789@naver.com>
2026-05-20 16:30:21 +0900

    feat: inner merger

--- Pandas/pandas_merge_conversion .py
+++ Pandas/pandas_merge_conversion .py
@@ -8,7 +8,7 @@ df2 = pd.DataFrame({'C': ['C0', 'C1', 'C2'],
                     'D': ['D0', 'D1', 'D2'] })
 
 df3 = pd.concat([df1, df2], axis=0) # 행 병합
-df4 = pd.concat([df1, df2], axis=1) # 행 병합
+df4 = pd.concat([df1, df2], axis=1) # 열 병합
 
 print(df1)
 print('\n')
@@ -18,3 +18,23 @@ print(df3)
 print('\n')
 print(df4)
 print('\n')
+
+print('-'*50)
+
+# Inner Merge
+df1 = pd.DataFrame({'key':['K0','K1','K2','K3'],
+                    'A': ['A0', 'A1', 'A2'],
+                    'B': ['B0', 'B1', 'B2'] })
+
+
+df1 = pd.DataFrame({'key':['K0','K1','K2'],
+                    'C': ['C0', 'C1', 'C2'],
+                    'D': ['D0', 'D1', 'D2'] })
+
+df2_merge = pd.merge(df1, df2, on='key', how='inner')
+
+print(df1)
+print('\n')
+print(df2)
+print('\n')
+print('df_merged')
